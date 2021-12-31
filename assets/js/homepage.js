1212432c5e4c844367247359b3c7201c35a12c89
aziz <ahmed613@umn.edu>
2021-12-31 02:09:27 -0600

    capture data

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,8 @@
 /*let getUserRepos = function() {
     console.log('function was called')
 }*/
-
+let userFormEl = document.querySelector('#user-form')
+let nameInputEl = document.querySelector('#username')
 let getUserRepos = (user) => {
     // format the github api url
     let apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -12,5 +13,17 @@ let getUserRepos = (user) => {
         console.log(data);
       });
     });
-  };
-getUserRepos('azizahmed77')
+};
+
+let formSubmitHandler = (event) => {
+    event.preventDefault()
+    let username = nameInputEl.value.trim();
+
+    if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+    } else {
+    alert("Please enter a GitHub username");
+    }
+}
+userFormEl.addEventListener("submit", formSubmitHandler);
